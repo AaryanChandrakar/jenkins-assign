57b4fdf5e66dcc882f158f2a077125cd6852b415
Aaryan Chandrakar <146125867+AaryanChandrakar@users.noreply.github.com>
2025-10-18 18:53:20 +0530

    Update aaryan.py

--- aaryan.py
+++ aaryan.py
@@ -5,3 +5,5 @@ print(result) # output: [99]
 nums = [22, 3, 4, 55, 6, 77, 8, 99, 10]
 result2 = [num for num in nums if(num%2==0 and num%3==0)]
 print(result2) # output: [6]
+
+print("Some Changes")
