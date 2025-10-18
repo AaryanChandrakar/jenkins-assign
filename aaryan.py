6350e39b09faec7363c0b369051802ea4152fb64
AaryanChandrakar <aryanchandrakar100@gmail.com>
2025-10-18 14:42:03 +0530

    new feature is added

--- aaryan.py
+++ aaryan.py
@@ -1,3 +1,7 @@
 nums = [22, 3, 4, 55, 6, 77, 8, 99, 10]
 result = [num for num in nums if(num %3 ==0 and num %9 ==0)]
 print(result) # output: [99]
+
+nums = [22, 3, 4, 55, 6, 77, 8, 99, 10]
+result2 = [num for num in nums if(num%2==0 and num%3==0)]
+print(result2) # output: [6]
